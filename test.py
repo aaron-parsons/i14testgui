129a77e69a944be786cd5534bb6d44e8fc01af4a
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2017-07-05 12:06:15 +0100

    initial attempt

---
+++ test.py
@@ -0,0 +1,34 @@
+'''
+testing some stuff
+'''
+
+# from content import Content
+# 
+# process_list ='/dls/science/users/clb02321/DAWN_stable/Savu/Savu/test_data/test_process_lists/pymca_test.nxs'
+# model = Content()
+# model.fopen(process_list)
+# model.modify('1', 'x', 'cheese')
+
+# class ControllerFactory(object):
+#     def __init__(self, model, plugin_number, key, widget):
+#         self.model = model
+#         self.plugin_number = plugin_number
+#         self.key = key
+#         self.widget = widget
+#     
+#     def controller(self):
+#         widgetvalue = str(self.widget.value())
+# #         print "wv", widgetvalue
+#         self.model.modify(self.plugin_number, self.key, widgetvalue)
+
+
+
+def get_visits():
+
+    import getpass
+    from subprocess import Popen, PIPE
+    
+    p = Popen(['sh','/dls_sw/apps/mx-scripts/visit_tools/getVisitsForFedId',getpass.getuser()], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+    rc = p.returncode
+    return output
